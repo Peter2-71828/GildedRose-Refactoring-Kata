d3a1ef4c42a4683b5506ab1ee76da22dc52acf0c
Peter <pc.dean@icloud.com>
2020-03-26 18:01:07 +0000

    write test to check Aged Brie and Sulfuras behaviour as well as quality not increasing above 50

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -21,4 +21,30 @@ describe("Gilded Rose", function() {
     expect(itemAfterOneDay[0].quality).to.equal(0);
   });
 
+  it("Aged Brie should increase in quality", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 5, 15) ]);
+    const itemAfterOneDay = gildedRose.updateQuality();
+    expect(itemAfterOneDay[0].quality).to.equal(16);
+  });
+
+  it("Aged Brie increases in quality twice as fast after sell by date", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", -1, 20) ]);
+    const itemAfterOneDay = gildedRose.updateQuality();
+    expect(itemAfterOneDay[0].quality).to.equal(22);
+  });
+
+  // given code allows for entry of quality greater than 50
+  it("quality of an item is never more than 50", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 10, 50) ]);
+    const itemAfterOneDay = gildedRose.updateQuality();
+    expect(itemAfterOneDay[0].quality).to.equal(50);
+  });
+
+  it("properties of Sulfuras should not change", function() {
+    const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 5, 30) ]);
+    const itemAfterOneDay = gildedRose.updateQuality();
+    expect(itemAfterOneDay[0].quality).to.equal(30);
+    expect(itemAfterOneDay[0].sellIn).to.equal(5);
+  });
+
 });
