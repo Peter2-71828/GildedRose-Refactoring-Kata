af491210a4fd5ef00922206663fda16598bd5ee4
Peter <pc.dean@icloud.com>
2020-03-26 14:46:46 +0000

    write test to check quality degrades twice as fast after sell by date

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -3,14 +3,20 @@ var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
   it("item quality and sellIn should reduce one", function() {
-    const gildedRose = new Shop([ new Item("foo", 3, 2) ]);
+    const gildedRose = new Shop([ new Item("foo", 1, 3) ]);
+    const itemAfterOneDay = gildedRose.updateQuality();
+    expect(itemAfterOneDay[0].sellIn).to.equal(0);
+    expect(itemAfterOneDay[0].quality).to.equal(2);
+  });
+
+  it("quality degrades twice as fast after sell by date", function(){
+    const gildedRose = new Shop([ new Item("foo", 0, 3) ]);
     const itemAfterOneDay = gildedRose.updateQuality();
     expect(itemAfterOneDay[0].quality).to.equal(1);
-    expect(itemAfterOneDay[0].sellIn).to.equal(2);
   });
 
   it("item quality should never be negative", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const gildedRose = new Shop([ new Item("foo", 2, 0) ]);
     const itemAfterOneDay = gildedRose.updateQuality();
     expect(itemAfterOneDay[0].quality).to.equal(0);
   });
