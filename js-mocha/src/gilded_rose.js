e26360b33e4d2b13c1ac7372ec7a43120968bf65
Peter <pc.dean@icloud.com>
2020-03-26 20:48:27 +0000

    added comment to explain code layout

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -23,6 +23,8 @@ class Shop {
   }
 }
 
+// Logic for updateQuality method for all the types of items
+
 function standardItems(item) {
   if (item.quality > 0) {
     item.quality--
@@ -56,33 +58,3 @@ module.exports = {
   Item,
   Shop
 }
-
-// if (this.items[i].sellIn < 6) {
-//   if (this.items[i].quality < 50) {
-//     this.items[i].quality = this.items[i].quality + 1;
-//   }
-// }
-// }
-// }
-// }
-// if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-// this.items[i].sellIn = this.items[i].sellIn - 1;
-// }
-// if (this.items[i].sellIn < 0) {
-// if (this.items[i].name != 'Aged Brie') {
-// if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-// if (this.items[i].quality > 0) {
-//   if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-//     this.items[i].quality = this.items[i].quality - 1;
-//   }
-// }
-// } else {
-// this.items[i].quality = this.items[i].quality - this.items[i].quality;
-// }
-// } else {
-// if (this.items[i].quality < 50) {
-// this.items[i].quality = this.items[i].quality + 1;
-// }
-// }
-// }
-// }
