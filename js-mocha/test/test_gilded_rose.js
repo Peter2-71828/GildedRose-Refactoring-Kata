2cd599d47eb9a2bc000e304dd41c863fc6837a1f
Peter <pc.dean@icloud.com>
2020-03-26 14:06:41 +0000

    write test to check for quality and sellIn reduction by one

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -2,6 +2,13 @@ var {expect} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
+  it("item quality and sellIn should reduce one", function() {
+    const gildedRose = new Shop([ new Item("foo", 3, 2) ]);
+    const itemAfterOneDay = gildedRose.updateQuality();
+    expect(itemAfterOneDay[0].quality).to.equal(1);
+    expect(itemAfterOneDay[0].sellIn).to.equal(2);
+  });
+
   it("item quality should never be negative", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const itemAfterOneDay = gildedRose.updateQuality();
